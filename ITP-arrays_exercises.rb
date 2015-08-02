d2fecd5f0f85f5752f64c74b82be188c9ebb8001
Jason Pilz <jasonpilz@gmail.com>
2015-08-01 22:09:08 -0700

    Added arrays file.

---
+++ ITP-arrays_exercises.rb
@@ -0,0 +1,29 @@
+
+# 1
+arr = [1, 3, 5, 7, 9]
+bool = arr.include?(3)
+puts bool
+
+# 2
+
+# 3
+arr = [["test", "hello", "world"],["example", "mem"]]
+puts arr.last.first
+
+# 4
+
+# 1 - 8
+# 2 -
+
+# 6
+
+# Trying to access a element in an array using a string..cannot convert a string into an int.
+
+# 7
+
+values = [34, 56, 2, 4, 5]
+
+values_new = values.map { |num| num + 2}
+
+p values
+p values_new
